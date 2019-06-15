d65035503c0d06dae753be9524433933a0ba49bc
Neraud <neraud.login@gmail.com>
2019-06-15 15:25:28 +0200

    Add missing requests dependency

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     long_description=read("README.md"),
     packages=find_packages(exclude=('tests', )),
     install_requires=[
+        'requests==2.22.0',
         'pyyaml==5.1.1',
         'Click==7.0',
         'www-authenticate==0.9.2',
